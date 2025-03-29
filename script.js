c1903c4e712874c136d46eff9c599d2930d8dd56
sathyam1975 <likhi.work@gmail.com>
2025-03-29 23:37:29 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+
+document.getElementById("linkedin").href = "https://www.linkedin.com/in/chalapati-likhit-narasimha-025320310";
+document.getElementById("github").href = "https://github.com/Sathyam1975";
+document.getElementById("email").href = "mailto:Likhi.work@gmail.com";
